f5c68f06f7e5a9c5df30e4424e7d6e522065b285
Mark Brown <markbrown04@googlemail.com>
2022-05-15 10:25:10 +0100

    Update setDefaults.sh

--- setDefaults.sh
+++ setDefaults.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Change default screenshot dir, move to trash automatically
 mkdir -p /Users/mark/Pictures/Screenshots;
 defaults write com.apple.screencapture location /Users/mark/Pictures/Screenshots
+cp automation/com.mark.clean-screenshots-dir.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/com.mark.clean-screenshots-dir.plist
