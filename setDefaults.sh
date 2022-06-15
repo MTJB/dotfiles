8ca668537f37597381f4de37fb7a2da42c8f7f60
Mark Brown <markbrown04@googlemail.com>
2022-06-15 08:24:30 +0100

    Update setDefaults.sh

--- setDefaults.sh
+++ setDefaults.sh
@@ -4,4 +4,6 @@
 mkdir -p /Users/mark/Pictures/Screenshots;
 defaults write com.apple.screencapture location /Users/mark/Pictures/Screenshots
 cp automation/com.mark.clean-screenshots-dir.plist ~/Library/LaunchAgents
-launchctl load -w ~/Library/LaunchAgents/com.mark.clean-screenshots-dir.plist
+sudo chown root:wheel ~/Library/LaunchAgents/com.mark.clean-screenshots-dir.plist # After Big Sur, need to transfer ownershipto root
+sudo chmod 600 ~/Library/LaunchAgents/com.mark.clean-screenshots-dir.plist
+sudo launchctl load -w ~/Library/LaunchAgents/com.mark.clean-screenshots-dir.plist
